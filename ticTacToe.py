f24e3a6bce88732ca260bab903518cbbae611504
Langdon Douglas <langdon.douglas26@gmail.com>
2024-10-02 11:00:28 -0500

    brainstorming display to user

---
+++ ticTacToe.py
@@ -0,0 +1,13 @@
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+exampleRow = [1,2,3]
+row1 = ["", "", ""]
+row2 = ["", "", ""]
+row3 = ["", "", ""]
+
+row2[1] = "X"
+
+display(row1,row2,row3)
